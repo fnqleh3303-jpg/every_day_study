1ed57131eaa421ac7217d3c7f535e37b06c842a2
fnqleh3303-jpg <fnqelh3303@naver.com>
2026-03-15 18:37:45 +0900

    [260315]양은하_알고리즘응용swea문제

---
+++ [0315] 양은하_알고리즘응용swea문제/Swea수영장.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+ 
+public class Swea수영장 {
+     
+    static int ans, d, m1, m3, y;
+    static int[] month;
+     
+     
+    public static void main(String[] args) {
+         
+    Scanner sc = new Scanner(System.in);
+     
+    int T= sc.nextInt();
+    for(int tc=1;tc<=T;tc++) {
+        d = sc.nextInt();
+        m1 = sc.nextInt();
+        m3 = sc.nextInt();
+        y = sc.nextInt();
+         
+        month = new int[13];//한달에 하나씩 인덱스 1부터 채운다
+         
+        for(int m=1;m<13;m++) {
+            month[m] = sc.nextInt();
+        }//할당 끝
+         
+        ans=y;//ans 초기화 해줘야지(최소값을 찾는거니까 1년권으로)
+        fee(1, 0);
+ 
+        System.out.println("#"+tc+" "+ans);
+         
+    }//테케 끝
+    }//메인 끝
+     
+    static void fee(int m, int fee) {
+        //종료
+        if(m>=13) {
+            ans = Math.min(ans, fee);//ans가 계속 갱신되는거니까 ans랑 fee랑 비교.
+        return;//아오 좀 익혀라
+        }//*return도 if문 안에 넣어야지 */
+         
+        //재귀
+        fee(m+1, fee + Math.min(month[m]*d, m1));
+ 
+        fee(m+3, fee+m3);
+         
+    }//메인 끝
+}//클래스 끝
---
+++ [0315] 양은하_알고리즘응용swea문제/Swea햄버거다이어트.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Swea햄버거다이어트 {
+	
+	static int num;
+	static int maxCal;
+	static int[][]total;	
+	
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	
+	int T = sc.nextInt();
+	for(int tc=1;tc<=T;tc++) {
+		num = sc.nextInt();
+		maxCal = sc.nextInt();
+		
+		total = new int[num][2];
+		for(int i=0;i<num;i++) {
+			total[i][0] = sc.nextInt();//점수
+			total[i][1] = sc.nextInt();		//칼로리	
+		}//할당 끝
+		int cal =0;
+		
+		int res = best(0, 0, 0);
+		
+		System.out.println("#"+ tc +" "+res);
+		
+	}//테케 끝
+}//메인 끝
+private static int best(int idxNum, int cal, int currentScore) {
+	if(cal>maxCal) {//*넘으면 그냥 0 return해서 실패한걸 보여줘
+		return 0;
+	}
+	
+	if(idxNum==num) {//*마지막까지 돌았는데 안터지면 그게 최대 값이니까
+		return currentScore;
+	}
+	int a= best(idxNum+1, cal, currentScore);
+	int b = best(idxNum+1, cal+total[idxNum][1], currentScore+total[idxNum][0]);
+	
+	return Math.max(a, b);//*여기 return을 더 큰 값으로 해야지. 
+}
+
+}
+//11시 11분
+//재귀에서 컨트롤해야하는건
+//칼로리가 max보다 위면 return해야한다.
+
